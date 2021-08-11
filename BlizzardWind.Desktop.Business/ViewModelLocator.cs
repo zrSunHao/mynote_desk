18a871eca5e86429a842fe68c5b0f152b835f8b2
sunhao <2080680175@@qq.com>
2021-08-11 19:07:21 +0800

    添加项目文件。

---
+++ BlizzardWind.Desktop.Business/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using BlizzardWind.Desktop.Business.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlizzardWind.Desktop.Business
+{
+    public partial class ViewModelLocator
+    {
+        private readonly IServiceCollection ServiceCollection;
+        private readonly ServiceProvider _serviceProvider;
+        public ViewModelLocator()
+        {
+            ServiceCollection = new ServiceCollection();
+            ConfigureServices(ServiceCollection);
+            _serviceProvider = ServiceCollection.BuildServiceProvider();
+        }
+    }
+
+    public partial class ViewModelLocator
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
+
+        public MainWindowViewModel? MainWindowViewModel => _serviceProvider.GetService<MainWindowViewModel>();
+    }
+}
