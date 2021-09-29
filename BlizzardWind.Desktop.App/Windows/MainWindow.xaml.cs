f3e9d302eaf5fb87f953c198b74ab39a4e2a5ed3
sunhao <2080680175@@qq.com>
2021-09-29 20:45:40 +0800

    更新主界面ui，未完成

--- BlizzardWind.Desktop.App/Windows/MainWindow.xaml.cs
+++ BlizzardWind.Desktop.App/Windows/MainWindow.xaml.cs
@@ -26,5 +26,31 @@ namespace BlizzardWind.Desktop.App.Windows
         {
             InitializeComponent();
         }
+
+        private void DragMove_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
